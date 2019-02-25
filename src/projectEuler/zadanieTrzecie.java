3468ad14564af8756d3683ac94ac837e685f2fa7
EwaKlepacz <ewa.elzbieta.klepacz@gmail.com>
2019-02-25 18:08:54 +0100

    sprawdzaenie czy liczba pierwsza BigInteger

--- src/projectEuler/zadanieTrzecie.java
+++ src/projectEuler/zadanieTrzecie.java
@@ -1,20 +1,22 @@
 package projectEuler;
 
+import java.math.BigInteger;
+
 public class zadanieTrzecie {
 
 	public static void main(String[] args) {
-		System.out.println((czyJestLiczbaPierwsza(24)));
+		System.out.println((czyJestLiczbaPierwsza(new BigInteger("24"))));
 		wyswietlPodzielniki(24);
 
 	}
 
-	public static boolean czyJestLiczbaPierwsza(int liczba) {
+	public static boolean czyJestLiczbaPierwsza(BigInteger liczba) {
 
 		boolean result = false;
-		double limit = Math.sqrt(liczba);
-		int i;
-		for (i = 2; i <= limit; i++) {
-			if (liczba % i == 0) {
+		BigInteger limit = liczba.divide(new BigInteger("2"));
+		BigInteger i = new BigInteger("2");
+		for (; i.compareTo(limit) <= -1; i.add(BigInteger.ONE)) {
+			if (liczba.remainder(i).equals(0)) {
 				return result;
 			}
 		}
@@ -23,11 +25,10 @@ public class zadanieTrzecie {
 
 	public static void wyswietlPodzielniki(int liczba) {
 		int i;
-		for (i = 1; i <= liczba; i++)
-			;
-		if (liczba % i == 0) {
-			System.out.println(i + "");
-
+		for (i = 1; i <= liczba; i++) {
+			if (liczba % i == 0) {
+				System.out.println(i + "");
+			}
 		}
 	}
 }
